227a0ef6fa97ff94ccc1d32867571bb28f3d4929
André Chalhub <achalhub@gmail.com>
2025-04-25 07:22:33 -0300

    Adiciona nova regra 'Bug no App' com escolha de 2 dias para bloqueio de pontos

--- script.js
+++ script.js
@@ -6,13 +6,14 @@ function toggleRules() {
 function sortPower() {
     const powers = [
         "Replay do Treino: repita 1 ponto da semana passada.",
+        "Modo Lendário: só pontua com check-in criativo na próxima semana.",
         "Acordei no Hype: check-in até 8h vale 2 pontos.",
         "Treino da Madrugada: check-in entre 0h-6h vale +1 ponto.",
         "Treino em Dobro: 2 check-ins em um dia, 2 pontos.",
+        "Peso da Coroa: um check-in da próxima semana não contará ponto.",
         "Meta Oculta: 5 check-ins na semana = +1 ponto extra.",
         "Treino Premiado: sorteie 1 a 3, faça check-ins seguidos e ganhe pontos.",
-        "Peso da Coroa: um check-in da próxima semana não contará ponto.",
-        "Modo Lendário: só pontua com check-in criativo na próxima semana."
+        "Bug no App: O jogador que invocou esta carta escolhe 2 dias da próxima semana nos quais os check-ins dos outros jogadores não contarão ponto."
     ];
 
     const powerNumber = Math.floor(Math.random() * powers.length);
